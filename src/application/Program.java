78677fd1db76b9ced02ab70fe36a2936f4646d35
Joao Menezes <joaovitormenezesm12@gmail.com>
2022-10-13 15:47:54 -0300

    project created

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+
+	}
+
+}
